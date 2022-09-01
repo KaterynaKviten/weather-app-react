04ac972a803ec1a8796e1a83363521b53c6b464f
Kateryna Shyurkova <katerina.shyurkova@gmail.com>
2022-09-01 15:38:35 +0300

    added link github repository

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Search />
+      <p>
+        <a href="https://github.com/KaterynaKviten/weather-app-react">
+          My github repository
+        </a>
+      </p>
     </div>
   );
 }
